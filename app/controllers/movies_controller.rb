4cc6c503b95b3d24fd8bd1d213c167bff2d34acb
yeye8863 <yyluanqibazao@163.com>
2016-10-18 22:13:49 -0500

    part1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.movies(params[:order_by])
+ 
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  #attr_accessible :title, :rating, :description, :release_date
+  def self.movies(sort_field)
+    self.order(sort_field)
+  end
 end
